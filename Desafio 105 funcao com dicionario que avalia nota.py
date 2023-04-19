7f633ba2a8ae70b9aec6b3c54e9057449a2ea402
Alex Y Suzuki <123485435+alexysbr@users.noreply.github.com>
2023-04-19 00:04:03 -0300

    desafio 105 terminado

--- Desafio 105 funcao com dicionario que avalia nota.py
+++ Desafio 105 funcao com dicionario que avalia nota.py
@@ -3,7 +3,8 @@ def notas(*n, sit=False):
     ->Função para analisar notas e situacoes de varios alunos.
     :param n: uma ou mais notas dos alunos (aceita varias)
     :param sit: valor opcional, indicando se deve ou nao adicionar a situacao do aluno
-    :return: dicionario com varias informacoes sobre a situacao da turma."""
+    :return: dicionario com varias informacoes sobre a situacao da turma.
+    """
 
     r = dict()
     r['total'] = len(n)
@@ -13,8 +14,16 @@ def notas(*n, sit=False):
     if sit:
         if r['media'] >= 7:
             r['situacao'] = 'BOA'
-    elif r['media'] >= 5:
-        r['situacao'] = 'RAZOÁVEL'
-    else:
-        r['situacao'] = 'RUIM'
+        elif r['media'] >= 5:
+            r['situacao'] = 'RAZOÁVEL'
+        else:
+            r['situacao'] = 'RUIM'
     return r
+
+#Programa principal
+resp = notas(5.5, 2.5, 1.5, sit=True)
+print()
+print(resp)
+print()
+help(notas)
+print()
