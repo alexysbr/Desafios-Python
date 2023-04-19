815bbd079b015086cf170f0b5813ea41530512c3
Alex Y Suzuki <123485435+alexysbr@users.noreply.github.com>
2023-04-18 23:50:57 -0300

    desafio 105 py

---
+++ Desafio 105 funcao com dicionario que avalia nota.py
@@ -0,0 +1,6 @@
+def notas(*n, sit=False):
+    """
+    ->Função para analisar notas e situacoes de varios alunos.
+    :param n: uma ou mais notas dos alunos (aceita varias)
+    :param sit: valor opcional, indicando se deve ou nao adicionar a situacao do aluno
+    :return: dicionario com varias informacoes sobre a situacao"""
